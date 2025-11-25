0b82c06d8719341b8759a082401f2f6d9716b370
Aleks-Ti <mti61ro@yandex.ru>
2025-11-26 01:06:16 +0300

    добавил немного кода для client -> clap и парс аргументов командной строки

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 ```bash
 cargo run --bin server
 ```
+
+## Клиентский сервер
+
+```bash
+cargo run --bin client -- --server-addr 127.0.0.1:8080 --udp-port 34254 --tickers-file tickers.txt
+```
--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,12 +1,34 @@
-use quotes_stream::BASE_SERVER_TCP_URL;
-use std::net::TcpListener;
-
+use clap::Parser;
 pub struct TcpClient {}
 
 impl TcpClient {
     pub fn send() {}
 }
 
-fn main() {
-    let _ = TcpListener::bind(BASE_SERVER_TCP_URL);
+#[derive(Parser, Debug, Clone)]
+struct Command {
+    /// Введите адрес на котором будет работать client. Пример: 127.0.0.1:8080,
+    #[arg(short = 'S', long("server-addr"))]
+    server_addr: String,
+    /// Укажите udp port на котором хотели бы прослушивать стриминг данных.
+    #[arg(short = 'U', long("udp-port"))]
+    udp_port: String,
+    /// Укажите путь до файла с катировками, для которых хотели бы получать данные.
+    /// Формат данных в файле: path/to/tickers.txt => AAPL\nMSFT\nGOOGL\nAMZN => каждый фильтр с новой строки.
+    #[arg(short = 'T', long("tickers-file"))]
+    tickers_path: String,
+}
+
+// cargo run --bin client -- --server-addr 127.0.0.1:8080 --udp-port 34254 --tickers-file test_tickers.txt
+fn main() -> std::io::Result<()> {
+    let args = Command::parse();
+    let data = std::fs::read_to_string(&args.tickers_path)?;
+    let tickers = data
+        .lines()
+        .map(|s| s.trim().to_string())
+        .filter(|s| !s.is_empty())
+        .collect::<Vec<_>>();
+    println!("Tickers: {:?}", tickers);
+    println!("Joined: {}", tickers.join(","));
+    Ok(())
 }
